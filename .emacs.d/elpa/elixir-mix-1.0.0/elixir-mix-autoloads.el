2e3a0a93d9676f6e49b5886502e06d66ac7af0fd
Unknown <312roadrunner@gmail.com>
2018-01-22 15:26:06 +0300

    Initial .emacs directory

---
+++ .emacs.d/elpa/elixir-mix-1.0.0/elixir-mix-autoloads.el
@@ -0,0 +1,29 @@
+;;; elixir-mix-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "elixir-mix" "elixir-mix.el" (23137 56141 0
+;;;;;;  0))
+;;; Generated autoloads from elixir-mix.el
+
+(defvar global-elixir-mix-mode nil "\
+Non-nil if Global Elixir-Mix mode is enabled.
+See the `global-elixir-mix-mode' command
+for a description of this minor mode.")
+
+(custom-autoload 'global-elixir-mix-mode "elixir-mix" nil)
+
+(autoload 'global-elixir-mix-mode "elixir-mix" "\
+Toggle global-elixir-mix-mode to use elixir's mix build tool within emacs.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; elixir-mix-autoloads.el ends here
---
+++ .emacs.d/elpa/elixir-mix-1.0.0/elixir-mix-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elixir-mix" "1.0.0" "Emacs integration for Elixir's mix" 'nil)
