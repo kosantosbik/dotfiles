dee8cd60168454eb907bf294cfdc13ab5fb95f57
kosantosbik <312roadrunner@gmail.com>
2018-03-29 18:20:33 +0300

    Created emacs directory. Added init.el and myinit.org

---
+++ emacs/init.el
@@ -0,0 +1,28 @@
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+;; Bootstrap 'use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (py-autopep8 material-theme ein better-defaults ggtags iedit expand-region aggressive-indent beacon yasnippet-snippets jedi htmlize org ox-reveal which-key use-package try counsel auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherith ace-jump-face-foreground :height 3.0)))))
